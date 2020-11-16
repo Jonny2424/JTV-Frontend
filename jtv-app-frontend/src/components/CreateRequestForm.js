5b150e4a0ce71cd48ff777b167b3144256634b9a
jonnyv91 <vaflorjonathan@johndeere.com>
2020-11-15 23:51:45 -0600

    added input boxes with formik and material UI

--- jtv-app-frontend/src/components/CreateRequestForm.js
+++ jtv-app-frontend/src/components/CreateRequestForm.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { Formik, Form, Field } from "formik";
+import { Card, CardContent, TextField, FormControl, FormLabel, RadioGroup, FormControlLabel } from '@material-ui/core';
+import * as Yup from "yup";
 
 class CreateRequestForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      title: "",
-      img: "",
-      body: "",
-      city: ""
+      request_Fname: '',
+      request_Lname: '',
+      request_email: '',
+      request_car_year: null,
+      request_car_make: '',
+      request_car_model: '',
+      request_msg: '',
+      request_date: null,
+      requestTypeId: null,
+      workTypeId: null,
+      notARobot: false,
+
     }
   }
 
@@ -19,33 +30,50 @@ class CreateRequestForm extends Component {
 
   render() {
     return (
-      <form onSubmit={(e) => this.props.createPost(e, this.state)}>
-        <input
-          type="text"
-          name="title"
-          value={this.state.title}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="img"
-          value={this.state.img}
-          onChange={this.handleChange}
-        />
-        <input
-          type="textarea"
-          name="body"
-          value={this.state.body}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="city"
-          value={this.state.city}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Post a Post" />
-      </form>
+      <Card>
+        <CardContent>
+          <Formik initialValues={this.state} onSubmit={() => { }}>
+            {({ values }) => (
+              <Form>
+                <Field name="request_Fname" as={TextField} label="First Name" />
+                <Field name="request_Lname" as={TextField} label="Last Name" />
+                <Field name="request_email" as={TextField} label="Email" />
+                <Field name="request_car_year" type="number" as={TextField} label="Car Year" />
+                <Field name="request_car_make" as={TextField} label="Car Make" />
+                <Field name="request_car_model" as={TextField} label="Car Model" />
+                <Field name="request_msg" as={TextField} label="Message" multiline rows={5} />
+                <label>
+                  <Field type="radio" name="requestTypeId" value="1" />
+              Request Info
+            </label>
+                <label>
+                  <Field type="radio" name="requestTypeId" value="2" />
+              Schedule a Detail
+            </label>
+                {values.requestTypeId === "2" ? <TextField
+                  id="date"
+                  label="Preferred Dropoff"
+                  type="date"
+                  defaultValue="mm/dd/yyyy"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                /> : null}
+                <Field name="workTypeId" as="select">
+                  <option defaultValue>Select One</option>
+                  <option value={1}>Bronze</option>
+                  <option value={2}>Silver</option>
+                  <option value={3}>Gold</option>
+                </Field>
+                <Field type="checkbox" name="notARobot" />
+                
+
+                <pre>{JSON.stringify(values, null, 4)}</pre>
+              </Form>
+            )}
+          </Formik>
+        </CardContent>
+      </Card>
     )
   }
 }
--- jtv-app-frontend/src/components/webpageComponents/ContactUs.js
+++ jtv-app-frontend/src/components/webpageComponents/ContactUs.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import CreateRequestForm from '../CreateRequestForm'
 
 export default function Detailing() {
     return (
         <>
         <Header />
         <h1>Contact us</h1>
+        <CreateRequestForm />
         </>
     )
 }
