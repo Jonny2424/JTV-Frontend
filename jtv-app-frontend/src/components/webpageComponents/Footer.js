c6b4f2b31f606379c5c3c0131fee1e8328dd28e9
jonnyv91 <vaflorjonathan@johndeere.com>
2020-11-17 22:36:32 -0600

    added facebook link

--- jtv-app-frontend/src/components/webpageComponents/Footer.js
+++ jtv-app-frontend/src/components/webpageComponents/Footer.js
@@ -3,9 +3,12 @@ import { Navbar } from 'react-bootstrap';
 
 export default function Footer() {
     return (
-        <Navbar className="justify-content-center" fixed="bottom" bg="dark" style={{textAlign: "center", color: "white"}}>
+        <Navbar className="justify-content-space-between" fixed="bottom" bg="dark" style={{textAlign: "center", color: "white", justifyContent: "space-between"}}>
             <div>
                 <h2>Jon Vaflor &copy; 2020</h2>
+            </div>
+            <div>
+            <a href="https://www.facebook.com/JTV-Auto-Detailing-173222920002021/?view_public_for=173222920002021" target="_blank" style={{textDecoration: "none", color: "white", fontSize: "25px"}}>Facebook</a>
             </div>
         </Navbar>
     )
